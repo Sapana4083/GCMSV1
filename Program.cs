9dc89ced556461b1821417b6c95738a08729ce21
GCMS <GCMS@DEWANSHU>
2026-08-20 10:48:44 +0530

    program

--- Program.cs
+++ Program.cs
@@ -34,7 +34,7 @@ builder.Services.AddDistributedMemoryCache();
 
 builder.Services.AddSession(options =>
 {
-    options.IdleTimeout = TimeSpan.FromMinutes(60);
+    options.IdleTimeout = TimeSpan.FromMinutes(240); 
     options.Cookie.HttpOnly = true;
     options.Cookie.IsEssential = true;
     options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
@@ -49,58 +49,97 @@ builder.Services.AddSession(options =>
 //});
 
 
-
 builder.Services.AddScoped<OracleConnectionFactory>();
+
 builder.Services.AddScoped<IUserRepository, UserRepository>();
+
 builder.Services.AddScoped<IAuthService, AuthService>();
+
 builder.Services.AddScoped<IStateRepository, StateRepository>();
+
 builder.Services.AddScoped<IStateService, StateService>();
+
 builder.Services.AddScoped<IDistrictRepository, DistrictRepository>();
+
 builder.Services.AddScoped<IDistrictService, DistrictService>();
+
 builder.Services.AddScoped<IDivisionRepository, DivisionRepository>();
+
 builder.Services.AddScoped<IDivisionService, DivisionService>();
+
 builder.Services.AddScoped<IRoleRepository, RoleRepository>();
+
 builder.Services.AddScoped<IRoleService, RoleService>();
+
 builder.Services.AddScoped<IMenuRepository, MenuRepository>();
+
 builder.Services.AddScoped<IMenuService, MenuService>();
+
 builder.Services.AddScoped<IRoleMenuMappingRepository, RoleMenuMappingRepository>();
+
 builder.Services.AddScoped<IRoleMenuMappingService, RoleMenuMappingService>();
+
 builder.Services.AddScoped<ITehsilRepository, TehsilRepository>();
+
 builder.Services.AddScoped<ITehsilService, TehsilService>();
+
 builder.Services.AddScoped<ISdoRepository, SdoRepository>();
+
 builder.Services.AddScoped<ISdoService, SdoService>();
+
 builder.Services.AddScoped<ICourtDashboardRepository, CourtDashboardRepository>();
 
 builder.Services.AddScoped<ICourtTypeRepository, CourtTypeRepository>();
+
 builder.Services.AddScoped<ICourtTypeService, CourtTypeService>();
 
 builder.Services.AddScoped<IDepartmentService, DepartmentService>();
+
 builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
 
-builder.Services.AddScoped<ICourtGroupRepository,CourtGroupRepository>();
-builder.Services.AddScoped<ICourtGroupService,CourtGroupService>();
+builder.Services.AddScoped<ICourtGroupRepository, CourtGroupRepository>();
+
+builder.Services.AddScoped<ICourtGroupService, CourtGroupService>();
 
 builder.Services.AddScoped<ICasePurposeRepository, CasePurposeRepository>();
+
 builder.Services.AddScoped<ICasePurposeService, CasePurposeService>();
 
 builder.Services.AddScoped<ICasePurposeGroupRepository, CasePurposeGroupRepository>();
+
 builder.Services.AddScoped<ICasePurposeGroupService, CasePurposeGroupService>();
 
 builder.Services.AddScoped<ICaseRepository, CaseRepository>();
+
 builder.Services.AddScoped<ICaseService, CaseService>();
+
 builder.Services.AddScoped<ILinkedCaseFamilyRepository, LinkedCaseFamilyRepository>();
 
 builder.Services.AddScoped<IRcsatCaseUpdateRepository, RcsatCaseUpdateRepository>();
+
 builder.Services.AddScoped<ILinkedCaseRepository, LinkedCaseRepository>();
 
 builder.Services.AddScoped<ICaseTypeRepository, CaseTypeRepository>();
+
 builder.Services.AddScoped<ICaseTypeService, CaseTypeService>();
 
 builder.Services.AddScoped<IBenchTypeRepository, BenchTypeRepository>();
+
 builder.Services.AddScoped<IBenchTypeService, BenchTypeService>();
 
 builder.Services.AddScoped<ICaseSubjectRepository, CaseSubjectRepository>();
+
 builder.Services.AddScoped<ICaseSubjectService, CaseSubjectService>();
+
+builder.Services.AddScoped<IDesignationRepository, DesignationRepository>();
+
+builder.Services.AddScoped<IDesignationService, DesignationService>();
+
+builder.Services.AddScoped<IAdvocateRepository, AdvocateRepository>();
+
+builder.Services.AddScoped<IAdvocateService, AdvocateService>();
+
+
 
 //builder.Services.AddAuthentication(
 //    CookieAuthenticationDefaults.AuthenticationScheme)
@@ -146,6 +185,38 @@ builder.Services.AddAntiforgery(options =>
 });
 
 var app = builder.Build();
+
+
+// existing header-stripping + CSP middleware
+app.Use(async (context, next) => { /* your existing header logic */ await next(); });
+
+// blocked verbs
+var blockedVerbs = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+    { "OPTIONS", "TRACE", "HEAD", "DELETE", "PUT", "PATCH" };
+app.Use(async (context, next) =>
+{
+    if (blockedVerbs.Contains(context.Request.Method))
+    {
+        context.Response.StatusCode = StatusCodes.Status405MethodNotAllowed;
+        return;
+    }
+    await next();
+});
+
+// blocked file extensions
+var blockedExtensions = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+    { ".bak", ".tmp", ".swp", ".log", ".old", ".~", ".zip", ".rar", ".7z", ".tar", ".gz", ".trc" };
+app.Use(async (context, next) =>
+{
+    var extension = Path.GetExtension(context.Request.Path.Value ?? "");
+    if (!string.IsNullOrEmpty(extension) && blockedExtensions.Contains(extension))
+    {
+        context.Response.StatusCode = StatusCodes.Status404NotFound;
+        return;
+    }
+    await next();
+});
+
 app.Use(async (context, next) =>
 {
     context.Response.OnStarting(() =>
